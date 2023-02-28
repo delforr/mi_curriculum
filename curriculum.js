484682bcec05f1107b19a7ebf953fce3a7049d8a
Delfor <64510908+delforr@users.noreply.github.com>
2023-02-28 13:46:25 -0300

    Add files via upload

---
+++ curriculum.js
@@ -0,0 +1,27 @@
+document.getElementById('boton').onclick = function(){
+
+    
+
+    document.getElementById("info_1").innerHTML = "En el año 2020 al llegar la pandemia empece a estudiar diseño web en Youtube, estudiando HTML, CSS y JAVASCRIPT";
+};
+
+document.getElementById('boton_2').onclick = function(){
+
+    
+
+    document.getElementById("info_2").innerHTML = "El primer codigo que estudie fue MQL4 por que trabajaba en Forex y queria automatizar las operaciones de Compra y Venta.";
+};
+
+document.getElementById('boton_3').onclick = function(){
+
+    
+
+    document.getElementById("info_3").innerHTML = "Despues de estudiar MQL4 , inicie con Back-end estudiando JAVA  y a continuacion HTML, CSS y JAVASCRIPT";
+};
+
+document.getElementById('boton_4').onclick = function(){
+
+    
+
+    document.getElementById("info_4").innerHTML = "Mi objetivo es ser programador Full - Stack. Primero Estilos o Maquetado de paginas web y aplicaciones moviles. Luego estudiar Javascript, React y Node.js";
+};
